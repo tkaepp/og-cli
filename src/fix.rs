526548a24b2b5276a22e3df6f3444ecbf918ee92
mikebucher <mike.bucher@digitecgalaxus.ch>
2024-06-26 14:47:32 +0200

    first not working verions of reinstall

--- src/fix.rs
+++ src/fix.rs
@@ -1,6 +1,9 @@
 use crate::plugin::Plugin;
 use clap::{Args, Subcommand};
 use crate::doctor::{DoctorFailure, DoctorSuccess};
+use std::env;
+use std::fs;
+use std::process::Command;
 
 pub struct Fix;
 
@@ -14,6 +17,74 @@ impl Fix {
     pub fn run(cli: FixCommand) {
         match cli.command {
             FixSubcommands::Hello { name } => println!("Hi {name}"),
+            FixSubcommands::Reinstall => 
+            { 
+                println!("Reinstalling Dg Cli");
+                let os = env::consts::OS;
+                match os{
+                    "macos"=>{
+                        Command::new("rm")
+                            .arg("-rf ~/.dgrc")
+                            .status()
+                            .expect("rm does not work");
+                        Command::new("rm")
+                            .arg("-rf ~/.dg-cli")
+                            .status()
+                            .expect("rm does not work");
+                        Command::new("rm")
+                            .arg("-rf ~/.local/bin/dg")
+                            .status()
+                            .expect("rm does not work");
+                        println!("attempting to reinstall pipx");
+                        let uninstallstatus = Command::new("brew")
+                            .arg("uninstall")
+                            .arg("pipx")
+                            .status()
+                            .expect("brew command failed to start");
+
+                            println!("Uninstall finished with: {uninstallstatus}");
+
+                            let message = match fs::remove_dir_all("~/.local/pipx"){
+                                Ok(()) => ".dg-cli deleted",
+                                Err(_e) => "unknown error",
+                            };
+                            println!("{message}");
+
+                        let installstatus = Command::new("brew")
+                            .arg("install")
+                            .arg("pipx")
+                            .status()
+                            .expect("brew command failed to start");
+
+                            println!("Install finished with: {installstatus}");
+
+                        Command::new("curl")
+                            .arg("-sL")
+                            .arg("https://dgcli.platform.prod.int.devinite.com/install.py")
+                            .arg("-o")
+                            .arg("install.py")
+                            .status()
+                            .expect("curl command failed to start");
+                        
+                        let clistatus = Command::new("python3")
+                            .arg("install.py")
+                            .status()
+                            .expect("python3 command failed to start");
+
+                            println!("cli install finished with: {clistatus}");
+
+                    },
+                    "windows"=>{
+                        println!("Hi I'm on windows");
+                    },
+                    "linux"=>{
+                        println!("Hi I'm on linux");
+                    },
+                    _ =>{
+                        println!("InvalidOS, cancelling");
+                    },
+                }
+            },
         }
     }
 }
@@ -27,4 +98,5 @@ impl Plugin for Fix {
 #[derive(Subcommand, Debug)]
 enum FixSubcommands {
     Hello { name: String },
+    Reinstall,
 }
