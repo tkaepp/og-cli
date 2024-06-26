60e1c1d3956d04f517bfe71dd4e5bb25b0557a99
Karim Es-sami <karim.es-sami@digitecgalaxus.ch>
2024-06-26 14:51:49 +0200

    Use constant instead of clumsy config struct

--- src/sql.rs
+++ src/sql.rs
@@ -8,22 +8,19 @@ use bollard::models::{ContainerStateStatusEnum, HostConfig, PortBinding};
 use bollard::Docker;
 use clap::{Args, Subcommand};
 
-use crate::doctor::{DoctorFailure, DoctorSuccess};
 use crate::get_config;
-use crate::plugin::Plugin;
 
 pub struct Sql;
+
+const CONTAINER_NAME: &str = "mssql-local";
+const IMAGE_NAME: &str = "mcr.microsoft.com/azure-sql-edge:latest";
+const VOLUME_BINDING: &str = "sql-data:/var/opt/mssql:rw";
+const PORT: i32 = 1433;
 
 #[derive(Args, Debug)]
 pub struct SqlCommand {
     #[command(subcommand)]
     command: SqlSubcommands,
-}
-
-impl Plugin for Sql {
-    fn doctor(&self) -> Vec<Result<DoctorSuccess, DoctorFailure>> {
-        Vec::new()
-    }
 }
 
 #[derive(Subcommand, Debug)]
@@ -37,118 +34,110 @@ pub enum SqlSubcommands {
 impl Sql {
     pub async fn run(cli: SqlCommand) {
         let sql_cmd = cli.command;
-        let config = SqlConfiguration::init_sql_config();
         let docker = Docker::connect_with_local_defaults().unwrap();
-        let status = get_container_status(docker.clone(), config.clone()).await;
+        let status = get_container_status(docker.clone()).await;
 
         match sql_cmd {
             SqlSubcommands::Start => {
                 if status == RUNNING {
                     println!(
                         "Container {} is already running, nothing to do.",
-                        config.container_name
+                        CONTAINER_NAME
                     );
                     return;
                 }
-                start(docker, config, status).await;
+                start(docker, status).await;
             }
             SqlSubcommands::Stop => {
                 if status == EXITED {
                     println!(
                         "Container {} is already stopped, nothing to do.",
-                        config.container_name
+                        CONTAINER_NAME
                     );
                     return;
                 }
-                stop(docker, config).await;
+                stop(docker).await;
             }
             SqlSubcommands::Remove => {
                 if status == EMPTY {
                     println!(
                         "Container {} doesn't exist, nothing to remove.",
-                        config.container_name
+                        CONTAINER_NAME
                     );
                     return;
                 }
-                remove(docker, config).await;
+                remove(docker).await;
             }
             SqlSubcommands::Status => {
-                println!("Container {} status: {:?}", config.container_name, status)
+                println!("Container {} status: {:?}", CONTAINER_NAME, status)
             }
         }
     }
 }
 
-async fn remove(docker: Docker, config: SqlConfiguration) {
-    println!("Removing container {}...", config.container_name);
-    let _ = docker
-        .remove_container(config.container_name.as_ref(), None)
-        .await;
-    println!("Container {} removed ", config.container_name);
+async fn remove(docker: Docker) {
+    println!("Removing container {}...", CONTAINER_NAME);
+    let _ = docker.remove_container(CONTAINER_NAME.as_ref(), None).await;
+    println!("Container {} removed ", CONTAINER_NAME);
 }
 
-async fn start(docker: Docker, config: SqlConfiguration, status: ContainerStateStatusEnum) {
+async fn start(docker: Docker, status: ContainerStateStatusEnum) {
     if status == EMPTY {
         println!(
             "Container {} doesn't exist, container will be created and started...",
-            config.container_name
+            CONTAINER_NAME
         );
-        create_and_run_container(docker, config).await;
+        create_and_run_container(docker).await;
         return;
     }
 
     println!(
         "Container {} exists but was stopped, container will restart...",
-        config.container_name
+        CONTAINER_NAME
     );
-    restart_container(docker, config.container_name.clone()).await;
+    restart_container(docker).await;
 }
 
-async fn stop(docker: Docker, config: SqlConfiguration) {
-    println!("Stopping container {}...", config.container_name);
-    let _ = docker
-        .stop_container(config.container_name.as_ref(), None)
-        .await;
-    println!("Container {} stopped ", config.container_name);
+async fn stop(docker: Docker) {
+    println!("Stopping container {}...", CONTAINER_NAME);
+    let _ = docker.stop_container(CONTAINER_NAME.as_ref(), None).await;
+    println!("Container {} stopped ", CONTAINER_NAME);
 }
 
-async fn restart_container(docker: Docker, container_name: Box<str>) {
+async fn restart_container(docker: Docker) {
     docker
-        .restart_container(
-            container_name.as_ref(),
-            Some(RestartContainerOptions { t: 10 }),
-        )
+        .restart_container(CONTAINER_NAME, Some(RestartContainerOptions { t: 10 }))
         .await
         .unwrap();
 }
 
-async fn create_and_run_container(docker: Docker, config: SqlConfiguration) {
+async fn create_and_run_container(docker: Docker) {
     let pwd = &get_config().sql_password;
     let formatted_pwd = &format!("MSSQL_SA_PASSWORD={pwd}");
-    let env = vec![formatted_pwd.to_string(), "ACCEPT_EULA=Y".to_string()];
+    let env = vec![formatted_pwd, "ACCEPT_EULA=Y"];
 
     let port_bindings = {
         let mut map = HashMap::new();
         map.insert(
-            format!("{}/tcp", config.port),
+            format!("{}/tcp", PORT),
             Some(vec![PortBinding {
                 host_ip: Some("0.0.0.0".to_string()),
-                host_port: Some(config.port.to_string()),
+                host_port: Some(PORT.to_string()),
             }]),
         );
         map
     };
 
     let options = Some(CreateContainerOptions {
-        name: config.container_name.clone(),
+        name: CONTAINER_NAME,
         platform: None,
     });
 
     let creation_config = Config {
-        image: Some(config.image_name.into_string()),
+        image: Some(IMAGE_NAME),
         env: Some(env),
         host_config: Some(HostConfig {
-            binds: Some(vec![config.volume_binding.to_string()]),
+            binds: Some(vec![VOLUME_BINDING.to_string()]),
             port_bindings: Some(port_bindings),
             ..Default::default()
         }),
@@ -160,56 +149,21 @@ async fn create_and_run_container(docker: Docker, config: SqlConfiguration) {
         .await
         .unwrap();
     docker
-        .start_container(
-            &config.container_name.clone(),
-            None::<StartContainerOptions<String>>,
-        )
+        .start_container(CONTAINER_NAME, None::<StartContainerOptions<String>>)
         .await
         .unwrap();
     println!("Container {} created and started", result.id);
 }
 
-async fn get_container_status(
-    docker: Docker,
-    config: SqlConfiguration,
-) -> ContainerStateStatusEnum {
+async fn get_container_status(docker: Docker) -> ContainerStateStatusEnum {
     let mut filters = HashMap::new();
-    filters.insert("name", vec![config.container_name.as_ref()]);
+    filters.insert("name", vec![CONTAINER_NAME]);
 
     let inspect = docker
-        .inspect_container(config.container_name.as_ref(), None)
+        .inspect_container(CONTAINER_NAME.as_ref(), None)
         .await;
     match inspect {
         Ok(_) => inspect.unwrap().state.unwrap().status.unwrap(),
         Err(_) => EMPTY,
-    }
-}
-
-struct SqlConfiguration {
-    container_name: Box<str>,
-    volume_binding: Box<str>,
-    image_name: Box<str>,
-    port: i32,
-}
-
-impl Clone for SqlConfiguration {
-    fn clone(&self) -> SqlConfiguration {
-        SqlConfiguration {
-            container_name: self.container_name.clone(),
-            volume_binding: self.volume_binding.clone(),
-            image_name: self.image_name.clone(),
-            port: self.port,
-        }
-    }
-}
-
-impl SqlConfiguration {
-    fn init_sql_config() -> SqlConfiguration {
-        SqlConfiguration {
-            container_name: "mssql-local".into(),
-            image_name: "mcr.microsoft.com/azure-sql-edge:latest".into(),
-            volume_binding: "sql-data:/var/opt/mssql:rw".into(),
-            port: 1433,
-        }
     }
 }
