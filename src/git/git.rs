6e8860af87e46816b198155da7dfde68f448f759
Tobias Käppeli <tobias.kaeppeli@digitecgalaxus.ch>
2024-06-26 11:04:37 +0200

    get all public ssh keys

--- src/git/git.rs
+++ src/git/git.rs
@@ -1,3 +1,8 @@
+use std::ffi::OsStr;
+use std::fs::read_dir;
+
+use expanduser::expanduser;
+
 use crate::git;
 use crate::git::commands::GitSubCommands;
 
@@ -18,7 +23,36 @@ fn setup() -> Result<(), String> {
 }
 
 #[cfg(target_family = "unix")]
-fn check_ssh_keys() {}
+fn check_ssh_keys() -> Result<(), String> {
+    let ssh_dir = expanduser("~/.ssh").unwrap(); // move this check to the doctor
+    match read_dir(ssh_dir) {
+        Ok(ssh_dir) => {
+            for entry in ssh_dir {
+                match entry {
+                    Ok(e) => {
+                        match e.path().extension().and_then(OsStr::to_str) {
+                            Some("pub") => {
+                                println!("{}", e.path().to_str().unwrap());
+                            }
+                            _ => {}
+                        }
+                    }
+                    Err(_) => { println!("Error reading a directory entry") }
+                }
+            }
+        }
+        Err(e) => { println!("Error occured {}", e.to_string()) }
+    }
+    Ok(())
+    // ssh_key::public::
+    // let existing_keys = PrivateKey::
+    //
+    // let _ = Select::new()
+    //     .with_prompt("Select cluster to sync")
+    //     .items(&clusters)
+    //     .interact()
+    //     .unwrap();
+}
 
 #[cfg(target_family = "windows")]
 fn check_ssh_keys() {}
