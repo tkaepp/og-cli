1baf0ee4f3ec87ae40637228dc301d0459e80749
Tobias Käppeli <tobias.kaeppeli@digitecgalaxus.ch>
2024-06-26 14:37:10 +0200

    create og-ssh key

--- src/git/git.rs
+++ src/git/git.rs
@@ -4,7 +4,8 @@ use clap::builder::TypedValueParser;
 use dialoguer::MultiSelect;
 use expanduser::expanduser;
 use eyre::Result;
-use ssh_key::PublicKey;
+use ssh_key::{Algorithm, LineEnding, PrivateKey, PublicKey};
+use ssh_key::rand_core::OsRng;
 
 use crate::git;
 use crate::git::commands::GitSubCommands;
@@ -20,14 +21,13 @@ impl Git {
     }
 }
 
-fn setup() -> Result<(), String> {
-    check_ssh_keys();
-    Ok(())
+fn setup() -> Result<()> {
+    check_ssh_keys()
 }
 
 #[cfg(target_family = "unix")]
 fn check_ssh_keys() -> Result<()> {
-    let ssh_dir = expanduser("~/.ssh")?; // move this check to the doctor
+    let ssh_dir = expanduser("~/.ssh2")?; // move this check to the doctor
 
     let public_keys: Vec<PublicKey> = ssh_dir.read_dir()?
         .filter_map(|r| r.ok())
@@ -37,16 +37,39 @@ fn check_ssh_keys() -> Result<()> {
         .filter_map(|p| p.ok())
         .collect();
 
-    let selection = MultiSelect::new()
-        .with_prompt("Select ssh keys to use")
-        .items(&public_keys)
-        .interact()
-        .unwrap();
-    println!("You chose:");
+    if public_keys.is_empty() {
+        let creation_options = &["Azure Devops", "Github"];
+        let defaults = &[true, true];
+        let selection = MultiSelect::new()
+            .with_prompt("Should OG create ssh keys for you?")
+            .items(&creation_options[..])
+            .defaults(&defaults[..])
+            .interact()
+            .unwrap();
 
-    for i in selection {
-        println!("{}", public_keys[i].to_string());
+        if selection.is_empty() {
+            println!("No git platforms were selected. Abort key creation");
+            return Ok(());
+        }
+
+        let private_key_ed = PrivateKey::random(&mut OsRng, Algorithm::Ed25519)?;
+
+        private_key_ed.write_openssh_file(ssh_dir.join("og-ssh").as_path(), LineEnding::LF).expect("key could not be created");
+        private_key_ed.public_key().write_openssh_file(ssh_dir.join("og-ssh.pub").as_path()).expect("key could not be created");
+    } else {
+        let selection = MultiSelect::new()
+            .with_prompt("Select ssh keys to use")
+            .items(&public_keys)
+            .interact()
+            .unwrap();
+        println!("You chose:");
+
+        for i in selection {
+            println!("{}", public_keys[i].to_string());
+        }
     }
+
+
     Ok(())
 }
 
