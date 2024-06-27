b8bbcf5538ad9ca22859c8305f918f663aac939d
Tobias Käppeli <tobias.kaeppeli@digitecgalaxus.ch>
2024-06-27 12:14:26 +0200

    add default dg cli forward

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use clap::{Parser, Subcommand};
+use std::env;
+
+use clap::{CommandFactory, Parser, Subcommand};
 use eyre::Result;
 
+use og_cli::{config, search};
 use og_cli::dg::{DgCli, DgCommand};
 use og_cli::doctor::DoctorCommand;
 use og_cli::dotnet::{self, DotnetCommand};
@@ -13,14 +16,13 @@ use og_cli::mongo_db::{self, MongoDbCommand};
 use og_cli::search::SearchCommand;
 use og_cli::sql;
 use og_cli::sql::SqlCommand;
-use og_cli::{config, search};
 
 #[derive(Parser)]
 #[command(version, about)]
-#[clap(name = "dg cli")]
+#[clap(name = "og cli")]
 struct Cli {
     #[command(subcommand)]
-    command: Commands,
+    command: Option<Commands>,
 }
 
 #[derive(Subcommand)]
@@ -56,28 +58,57 @@ enum Commands {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let cli = Cli::parse();
     config::init_config().await?;
 
-    match cli.command {
-        Commands::MongoDb(mongodb_command) => mongo_db::MongoDb::run(mongodb_command),
-        Commands::Sql(sql_command) => sql::Sql::run(sql_command).await?,
-        Commands::Dotnet(command) => dotnet::Dotnet::run(command).expect("Reason"),
-        Commands::Git(git_command) => git::Git::run(git_command),
-        Commands::Fix(_) => {
-            fix::Fix::run()?;
+    let cli = Cli::try_parse();
+
+    match cli {
+        Ok(c) => {
+            match c.command {
+                Some(Commands::MongoDb(mongodb_command)) => mongo_db::MongoDb::run(mongodb_command),
+                Some(Commands::Sql(sql_command))=> sql::Sql::run(sql_command).await?,
+                Some(Commands::Dotnet(command)) => dotnet::Dotnet::run(command).expect("Reason"),
+                Some(Commands::Git(git_command)) => git::Git::run(git_command),
+                Some(Commands::Fix(_)) => {
+                    fix::Fix::run()?;
+                }
+                Some(Commands::Doctor(dr_command)) => og_cli::doctor::run(dr_command),
+                Some(Commands::Kubernetes(kubernetes_command)) => {
+                    kube::Kubernetes::run(kubernetes_command).await?
+                }
+                Some(Commands::GraphQl(graphql_command)) => {
+                    GraphQl::run(graphql_command)?;
+                }
+                Some(Commands::Search(search_command)) => search::Search::run(search_command).await?,// default is to forward unknown commands to the python dg cli
+                Some(Commands::Dg(dg_command)) => {
+                    DgCli::run(dg_command)?;
+                }
+                None => {
+                    let mut cmd = Cli::command();
+                    cmd.build();
+                    cmd.print_help();
+                    std::process::exit(0);
+                }
+            }
         }
-        Commands::Doctor(dr_command) => og_cli::doctor::run(dr_command),
-        Commands::Kubernetes(kubernetes_command) => {
-            kube::Kubernetes::run(kubernetes_command).await?
-        }
-        Commands::GraphQl(graphql_command) => {
-            GraphQl::run(graphql_command)?;
-        }
-        Commands::Search(search_command) => search::Search::run(search_command).await?,
-        // default is to forward unknown commands to the python dg cli
-        Commands::Dg(dg_command) => {
-            DgCli::run(dg_command)?;
+        Err(e) => {
+            let args: Vec<String> = env::args().skip(1).collect();
+
+            let mut cmd = Cli::command();
+            cmd.build();
+
+
+            if args.is_empty(){
+                e.exit();
+            }else if args.len() == 1 && args.iter().any(|x| x == "--help" || x == "-h"){
+                cmd.print_help();
+                std::process::exit(0);
+            }else if args.len() == 1 && args.iter().any(|x| x == "--version" || x == "-v"){
+                println!("{}", cmd.render_long_version());
+                std::process::exit(0);
+            }else{
+                DgCli::run_from_plain_args(args)?;
+            }
         }
     }
 
