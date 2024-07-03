928f3543dd69e608cf1bf64d4d2a942ea447b615
Danny Meier <danny.meier@digitecgalaxus.ch>
2024-07-03 18:04:06 +0200

    Implemented kube-beta init and doctor fixes

--- src/kube/kubernetes.rs
+++ src/kube/kubernetes.rs
@@ -1,12 +1,9 @@
 use clap::{Args, Subcommand};
 use colored::Colorize;
-use dialoguer::MultiSelect;
+use dialoguer::{Confirm, MultiSelect};
 use eyre::Context;
-use log::{error, info};
-use std::{
-    fmt::{Display, Formatter},
-    io::Read,
-};
+use log::{error, info, warn};
+use std::fmt::{Display, Formatter};
 
 use super::{kube_config, kube_config::*, rancher::*};
 
@@ -24,16 +21,28 @@ pub struct KubernetesCommand {
 
 #[derive(Subcommand, Debug)]
 pub enum KubernetesSubcommands {
+    /// Initializes your environment for the first use
+    Init {
+        /// Do not create a kubeconfig if not exists
+        #[arg(short = 'K', long)]
+        no_kubeconfig: bool,
+        /// Do not create a new Rancher API token
+        #[arg(short = 'T', long)]
+        no_rancher_token: bool,
+        /// Omits doing a backup of your kubeconfig
+        #[arg(short = 'B', long)]
+        no_backup: bool,
+    },
     /// Synchronizes the DG Rancher Kubernetes contexts
     Sync {
         /// Omits doing a backup of your kubeconfig
-        #[arg(short, long)]
+        #[arg(short = 'B', long)]
         no_backup: bool,
     },
     /// Cleanup (delete) local kubeconfig
     Cleanup {
         /// Omits doing a backup of your kubeconfig
-        #[arg(short, long)]
+        #[arg(short = 'B', long)]
         no_backup: bool,
     },
 }
@@ -48,6 +57,12 @@ impl KubernetesPlugin {
                 .context("Unable to sync clusters due to errors")?,
             KubernetesSubcommands::Cleanup { no_backup } => run_cleanup(!no_backup)
                 .context("Unable to cleanup local kubeconfig due to errors")?,
+            KubernetesSubcommands::Init {
+                no_kubeconfig,
+                no_rancher_token,
+                no_backup,
+            } => run_init(!no_kubeconfig, !no_rancher_token, !no_backup)
+                .context("Unable to initialize your local environment for first use")?,
         }
 
         Ok(())
@@ -93,9 +108,7 @@ impl Display for ClusterSyncAction {
 }
 
 async fn run_sync(kubeconfig_backup: bool) -> eyre::Result<()> {
-    info!("{}", "Depending on your OS, you have to confirm or enter your password to access the credential store to retrieve the necessary access tokens [ENTER]".bright_green());
-    let buffer = &mut [0u8];
-    std::io::stdin().read_exact(buffer).unwrap();
+    print_credential_store_warning();
 
     let rancher_token = get_rancher_token()?;
     let rancher_clusters = get_rancher_clusters(&rancher_token).await;
@@ -219,6 +232,41 @@ fn run_cleanup(kubeconfig_backup: bool) -> eyre::Result<()> {
         "{}",
         "Your local kubeconfig has been cleaned up successfully".green()
     );
+
+    Ok(())
+}
+
+fn run_init(
+    create_kubeconfig: bool,
+    create_rancher_token: bool,
+    kubeconfig_backup: bool,
+) -> eyre::Result<()> {
+    if create_kubeconfig {
+        let kubeconfig_result = read_kubeconfig();
+        match kubeconfig_result {
+            Ok(_) => info!(
+                "{}",
+                "An existing kubeconfig has been found. Creation skipped\n".green()
+            ),
+            Err(_) => create_empty_kubeconfig(kubeconfig_backup)?,
+        }
+    }
+
+    if create_rancher_token {
+        print_credential_store_warning();
+
+        let rancher_token_result = get_rancher_token();
+        match rancher_token_result {
+            Ok(_) => {
+                warn!("{}", "An existing Rancher API token has been found in the credential store. Overwrite?".yellow());
+                let confirmed = Confirm::new().default(false).interact_opt().unwrap();
+                if let Some(true) = confirmed {
+                    add_rancher_token()?;
+                }
+            }
+            Err(_) => add_rancher_token()?,
+        }
+    }
 
     Ok(())
 }
@@ -415,4 +463,8 @@ fn delete_kubeconfig_entry(local_cluster: &Cluster, kubeconfig_backup: bool) -> 
 
 fn get_cluster_fullname(cluster: &Cluster) -> String {
     format!("{}{}", cluster.name, cluster.name_suffix)
+}
+
+fn print_credential_store_warning() {
+    info!("Depending on your OS, you have to confirm or enter your password to access the credential store to retrieve the necessary access tokens\n");
 }
